8d4e3d63848331bdf94419453714a95f3becef44
Arshak Martirosyan <mat90x@mail.ru>
2020-01-22 00:04:28 +0300

    refactor command line arguments, use strokes, strict check for input format, customizable text margins and size, optional intermediary images saving, improve mosaics drawing algorithm

--- py/draw_mosaic.py
+++ py/draw_mosaic.py
@@ -7,103 +7,121 @@ from PIL import ImageFont
 from PIL import ImageDraw
 
 ap = argparse.ArgumentParser(description='Draw mosaic from captioned images.')
-ap.add_argument('-s', '--size', type=int, help='font size in pixels')
-ap.add_argument('-c', '--color', type=str, help='font color RGB code in hex')
-ap.add_argument('-d', '--dir', type=str, help='path to directory containing source images')
-ap.add_argument('-l', '--layout', type=str, help='dimensions for each mosaic')
+ap.add_argument('dir', type=str, metavar='DIR', help='path to directory containing images')
+ap.add_argument('dimension', type=str, metavar='DIMENSION', help='dimensions for each mosaic')
+ap.add_argument('font_size', type=int, metavar='FONT_SIZE', help='font size in pixels')
+ap.add_argument('-f', '--font_color', type=str, default='ff0000', help='font color RGB code in hex')
+ap.add_argument('-w', '--stroke_width', type=int, help='width for strokes around text')
+ap.add_argument('-s', '--stroke_color', type=str, default='ffffff', help='color for strokes around text')
+ap.add_argument('-t', '--margin_top', type=int, default=50, help='margin from top to the center of text')
+ap.add_argument('-l', '--margin_left', type=int, default=50, help='margin from left to the center of text')
 ap.add_argument('-r', '--resize', type=int, help='resize images height before stacking')
-ap.add_argument('-b', '--border', type=str, help='color for borders around text')
+ap.add_argument('-i', '--save_images', action='store_true', help='save intermediate images')
 args = ap.parse_args()
 
-font_size = args.size * 0.75 # point to pixel ratio
-font_color = tuple(int(args.color[i:i+2], 16) for i in range(0, 6, 2))
-images_dir = args.dir.rstrip('/')
-col, row = map(int, args.layout.split('x'))
-resize_height = args.resize
-border = args.border and tuple(int(args.border[i:i+2], 16) for i in range(0, 6, 2))
+args.dir = args.dir.rstrip('/')
+cols, rows = map(int, args.dimension.split('x'))
+args.font_size = int(args.font_size * 0.75) # point to pixel ratio
+font_color = tuple(int(args.font_color[i:i+2], 16) for i in range(0, 6, 2))
+stroke_color = tuple(int(args.stroke_color[i:i+2], 16) for i in range(0, 6, 2))
 
 images = {}
 row_number = 1
 for line in sys.stdin:
 
-    name, top, left, size, text = map(str.strip, line.split(':'))
+    try:
+        name, margin_top, margin_left, size, text = map(str.strip, line.split(':'))
+    except ValueError:
+        print(f'wrong format for row #{row_number}')
+        sys.exit(1)
 
     if not name:
         print(f'empty name for row #{row_number}')
-        continue
+        sys.exit(1)
 
-    if not text:
-        print(f'empty text value for row #{row_number}')
-        continue
-
-    top = not top and 0.5 or float(top) / 100
-    left = not left and 0.5 or float(left) / 100
-    size = int(size and int(size) * 0.75 or font_size)
+    margin_top, margin_left = args.margin_top / 100, args.margin_left / 100
+    size = int(size and int(size) * 0.75 or args.font_size)
 
     file, path = '', ''
     for extension in ['jpg', 'jpeg', 'png']:
-        if os.path.exists(images_dir + '/' + name + '.' + extension):
+        if os.path.exists(args.dir + '/' + name + '.' + extension):
             file = name + '.' + extension
             break
 
     if not file:
         print(f'file for "{name}" does not exist')
-        continue
+        sys.exit(1)
 
     if file not in images:
-        images[file] = [[top, left, size, text]]
+        images[file] = [[margin_top, margin_left, size, text]]
     else:
-        images[file].append([top, left, size, text])
+        images[file].append([margin_top, margin_left, size, text])
 
     row_number += 1
 
+mosaic = Image.new(mode='RGB', size=(15000, 15000))
+mosaic_count = 1
+paste_count = 0
+crop_x, crop_y = 0, 0
+paste_x, paste_y = 0, 0
+row_height = 0
 for file in images:
 
-    path = images_dir + '/' + file;
+    path = args.dir + '/' + file;
     try:
         image = Image.open(path)
     except IOError:
         print(f'could not open file "{path}" for captioning')
-        continue
+        sys.exit(1)
 
     draw = ImageDraw.Draw(image)
     image_width, image_height = image.size
 
     for draw_text in images[file]:
-        top, left, size, text = draw_text
+        margin_top, margin_left, size, text = draw_text
 
         font = ImageFont.truetype('DejaVuSans.ttf', size)
         font_width, font_height = font.getsize(text)
-        x, y = image_width * left - font_width / 2, image_height * top - font_height / 2
+        x = image_width * margin_left - font_width / 2
+        y = image_height * margin_top - font_height / 2
 
-        if border:
-            for offset in ([-1, +1], [+1, +1], [-1, -1], [+1, -1]):
-                draw.text((x + offset[0], y + offset[1]), text, border, font=font)
+        if args.stroke_width:
+            draw.text((x, y), text, font_color, font, stroke_fill=stroke_color, stroke_width=args.stroke_width)
+        else:
+            draw.text((x, y), text, font_color, font, stroke_fill=stroke_color, stroke_width=args.stroke_width)
 
-        draw.text((x, y), text, font_color, font=font)
+    if args.resize and args.resize != image_height:
+        image_width = int(image_width * args.resize / image_height)
+        image_height = args.resize
+        image = image.resize((image_width, image_height))
 
-    image.save(file)
+    if args.save_images:
+        image.save(file)
 
-mosaic = Image.new(mode='RGB', size=(15000, 15000))
-x, y, count, mosaic_count = 0, 0, 0, 1
-for file in images:
+    mosaic.paste(image, (paste_x, paste_y))
+    paste_count += 1
 
-    image = Image.open(file)
-    image_width, image_height = image.size
-    if resize_height and resize_height != image_height:
-        image_width = int(image_width * resize_height / image_height), resize_height
-        image = image.resize((image_width, resize_height))
+    paste_x += image_width
+    crop_x = max(crop_x, paste_x)
+    row_height = max(row_height, image_height)
 
-    mosaic.paste(image, (x, y))
-    count += 1
+    if paste_count % cols == 0:
+        crop_y += row_height
+        row_height = 0
 
-    x += image_width
+        if paste_count == cols * rows:
+            mosaic = mosaic.crop(box=(0, 0, crop_x, crop_y))
+            mosaic.save(f'mosaic{mosaic_count}.jpg')
+            mosaic_count += 1
 
-    if count == col * row:
-        mosaic = mosaic.crop(box=(0, 0, x, y + image_height))
-        mosaic.save(f'mosaic{mosaic_count}.jpg')
-        mosaic = Image.new(mode='RGB', size=(15000, 15000))
-        mosaic_count += 1
-        x, y, count = 0, 0, 0
-    elif count % col == 0:
-        x, y = 0, y + image_height
+            mosaic = Image.new(mode='RGB', size=(15000, 15000))
+            paste_x, paste_y = 0, 0
+            crop_x, crop_y = 0, 0
+            paste_count = 0
+
+        else:
+            paste_x, paste_y = 0, crop_y
+
+if paste_count:
+    mosaic = mosaic.crop(box=(0, 0, crop_x, crop_y + row_height))
+    mosaic.save(f'mosaic{mosaic_count}.jpg')
